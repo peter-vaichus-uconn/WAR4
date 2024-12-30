bd53167504f9fc62d3ffe1c401d80f886c35d0f4
Peter T. Vaichus <peterthomas@Peters-MacBook-Air-2.local>
2024-12-30 17:03:51 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,271 @@
+import { useState } from 'react'
+import background1 from "./Screenshot 2024-11-21 at 1.21.42 PM.png";
+import background2 from "./Screenshot 2024-11-21 at 1.29.16 PM.png";
+import ReactCardFlip from 'react-card-flip';
+import './App.css'
+
+import { deck1 }  from './faces';
+import { deck2 } from './faces'
+import { Queue } from './faces';
+
+const first1 = deck1.dequeue() // [2,H2]
+const first2 = deck2.dequeue() // [2,S2]
+
+function App() {
+
+  const [p1_count, SetCount1] = useState(26)
+  const [p2_count, SetCount2] = useState(26)
+
+  const [w1_count, SetWager1] = useState(6)
+  const [w2_count, SetWager2] = useState(6)
+
+
+  const [result, SetResult] = useState("")
+
+
+  const [isFlipped, SetIsFlipped] = useState(true)
+  const [available, SetAvailable] = useState(true)
+
+  //first two cards in the deck. ex: face1 = [2, C2]
+  const [face1, SetFace1] = useState(first1)
+  const [face2, SetFace2] = useState(first2)
+
+
+  //flip the card (change the state of isFlipped)
+  const flipCard = () => {
+    SetIsFlipped(!isFlipped); 
+  };
+
+  //only take a card if the card is removed from the pile
+  const flipAvailable = () => {
+    SetAvailable(!available); 
+  };
+
+
+  function decrement1(){
+    if(p1_count == 0){return}
+    SetCount1(p1_count-1)
+  }
+
+  function decrement2(){
+    if(p2_count == 0){return}
+    SetCount2(p2_count-1)
+  }
+
+  function compare(face1,face2){
+    if(face1[0] > face2[0]){
+      SetResult("Card 1 wins")
+      
+    }
+    else if(face1[0] < face2[0]){
+      SetResult("Card 2 wins")
+    }
+    else{
+      SetResult("WAR!")
+    }
+  }
+
+  function claim(temp1,temp2){
+
+    if(p1_count ==0){
+      SetResult("Player 2 Wins!")
+
+    }
+    else if(p2_count==0){
+      SetResult("Player 1 Wins!")
+    }
+    else{
+      SetResult("")
+    }
+   
+    SetFace1(deck1.dequeue())
+    SetFace2(deck2.dequeue())
+    
+    if(temp1[0] > temp2[0]){
+      deck1.enqueue(temp1)
+      deck1.enqueue(temp2)
+      SetCount1(p1_count+2)
+    }
+    else if(temp1[0] < temp2[0]){
+      deck2.enqueue(temp1)
+      deck2.enqueue(temp2)
+      SetCount2(p2_count+2)
+    }
+
+  }
+
+  // function wager(){
+
+  // }
+
+
+  return (
+    <>
+    <div className ="score">
+      <div>
+        <button className = "deal">{p1_count}</button>
+        <button className = "FC">{p2_count}</button>
+        
+      </div>
+      <div>
+        {/* <button className = "deal" onClick={increment1}>deal</button> */}
+        {available &&(<button className = "flip" onClick={() => { flipCard(); (available ? (decrement1(), decrement2(), flipAvailable(), compare(face1,face2)): flipAvailable())}}>flip</button>)}
+        {!available &&(<button className = "claim" onClick={() => { flipCard(); claim(face1,face2); (available ? (decrement1(), decrement2(), flipAvailable()): flipAvailable())}}>claim</button>)}
+        {!available &&(<button className = "claim" onClick={() => { flipCard(); claim(face1,face2); (available ? (decrement1(), decrement2(), flipAvailable()): flipAvailable())}}>wager</button>)}
+        <div className = "result">{result}</div>
+      </div>
+      
+    </div>
+
+    <div className="pile1">
+      {/* <div style={{backgroundImage: `url(${face1[1]})`,backgroundSize: "cover",}}className="card T TC"></div> */}
+
+      {p1_count > 51 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN26"></div>)}
+      {p1_count > 50 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN25"></div>)}
+      {p1_count > 49 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN24"></div>)}
+      {p1_count > 48 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN23"></div>)}
+      {p1_count > 47 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN22"></div>)}
+      {p1_count > 46 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN21"></div>)}
+      {p1_count > 45 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN20"></div>)}
+      {p1_count > 44 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN19"></div>)}
+      {p1_count > 43 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN18"></div>)}
+      {p1_count > 42 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN17"></div>)}
+      {p1_count > 41 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN16"></div>)}
+      {p1_count > 40 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN15"></div>)}
+      {p1_count > 39 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN14"></div>)}
+      {p1_count > 38 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN13"></div>)}
+      {p1_count > 37 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN12"></div>)}
+      {p1_count > 36 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN11"></div>)}
+      {p1_count > 35 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN10"></div>)}
+      {p1_count > 34 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN9"></div>)}
+      {p1_count > 33 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN8"></div>)}
+      {p1_count > 32 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN7"></div>)}
+      {p1_count > 31 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN6"></div>)}
+      {p1_count > 30 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN5"></div>)}
+      {p1_count > 29 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN4"></div>)}
+      {p1_count > 28 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN3"></div>)}
+      {p1_count > 27 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN2"></div>)}
+      {p1_count > 26 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TN1"></div>)}
+      
+      {p1_count > 25 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T1"></div>)}
+      {p1_count > 24 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T2"></div>)}
+      {p1_count > 23 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T3"></div>)}
+      {p1_count > 22 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T4"></div>)} 
+      {p1_count > 21 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T5"></div>)}
+      {p1_count > 20 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T6"></div>)}
+      {p1_count > 19 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T7"></div>)}
+      {p1_count > 18 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T8"></div>)}
+      {p1_count > 17 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T9"></div>)} 
+      {p1_count > 16 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T10"></div>)}
+      {p1_count > 15 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T11"></div>)}
+      {p1_count > 14 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T12"></div>)}
+      {p1_count > 13 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T13"></div>)}
+      {p1_count > 12 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T14"></div>)}
+      {p1_count > 11 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T15"></div>)}
+      {p1_count > 10 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T16"></div>)}
+      {p1_count > 9 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T17"></div>)}
+      {p1_count > 8 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T18"></div>)}
+      {p1_count > 7 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T19"></div>)}
+      {p1_count > 6 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T20"></div>)}
+      {p1_count > 5 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T21"></div>)}
+      {p1_count > 4 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T22"></div>)}
+      {p1_count > 3 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T23"></div>)}
+      {p1_count > 2 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T24"></div>)}
+      {p1_count > 1 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card T25"></div>)}
+      
+      {p1_count > -1 && (
+      <>
+      {p1_count > 0 && (<div style={{ backgroundImage: `url(${background1})`,backgroundSize: "cover"}} className= "card"></div>)}
+      <ReactCardFlip flipDirection="verticle" isFlipped={isFlipped}>
+        <div style={{ backgroundImage: `url(${face1[1]})`,backgroundSize: "cover"}} className= "card TC"></div>  
+        {p1_count > 100 && (<div style={{ backgroundImage: `url(${background1})`,backgroundSize: "cover"}} className= "card" ></div>)}
+      </ReactCardFlip>
+
+      {w1_count > 5 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TW1"></div>)}
+      {w1_count > 4 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TW2"></div>)}
+      {w1_count > 3 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TW3"></div>)}
+      {w1_count > 2 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TW4"></div>)}
+      {w1_count > 1 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TW5"></div>)}
+      {w1_count > 0 && (<div style={{backgroundImage: `url(${background1})`,backgroundSize: "cover",}}className="card TW6"></div>)}
+      
+      </>
+      )}
+    </div>
+    <div className="pile2">
+      {/* <div style={{backgroundImage: `url(${face2[1]})`,backgroundSize: "cover",}}className="card T BC"></div> */}
+      {p2_count > 51 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN26'></div>}
+      {p2_count > 50 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN25'></div>}
+      {p2_count > 49 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN24'></div>} 
+      {p2_count > 48 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN23'></div>}
+      {p2_count > 47 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN22'></div>}
+      {p2_count > 46 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN21'></div>}
+      {p2_count > 45 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN20'></div>}
+      {p2_count > 44 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN19'></div>}
+      {p2_count > 43 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN18'></div>}
+      {p2_count > 42 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN17'></div>}
+      {p2_count > 41 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN16'></div>}
+      {p2_count > 40 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN15'></div>}
+      {p2_count > 39 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN14'></div>}
+      {p2_count > 38 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN13'></div>}
+      {p2_count > 37 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN12'></div>}
+      {p2_count > 36 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN11'></div>}
+      {p2_count > 35 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN10'></div>}
+      {p2_count > 34 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN9'></div>}
+      {p2_count > 33 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN8'></div>}
+      {p2_count > 32 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN7'></div>}
+      {p2_count > 31 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN6'></div>}
+      {p2_count > 30 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN5'></div>}
+      {p2_count > 29 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN4'></div>}
+      {p2_count > 28 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN3'></div>}
+      {p2_count > 27 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN2'></div>}
+      {p2_count > 26 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card BN1'></div>}
+
+      {p2_count > 25 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B1'></div>}
+      {p2_count > 24 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B2'></div>}
+      {p2_count > 23 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B3'></div>}
+      {p2_count > 22 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B4'></div>}
+      {p2_count > 21 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B5'></div>}
+      {p2_count > 20 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B6'></div>}
+      {p2_count > 19 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B7'></div>}
+      {p2_count > 18 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B8'></div>}
+      {p2_count > 17 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B9'></div>}
+      {p2_count > 16 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B10'></div>}
+      {p2_count > 15 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B11'></div>}
+      {p2_count > 14 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B12'></div>}
+      {p2_count > 13 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B13'></div>}
+      {p2_count > 12 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B14'></div>}
+      {p2_count > 11 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B15'></div>}
+      {p2_count > 10 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B16'></div>}
+      {p2_count > 9 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B17'></div>}
+      {p2_count > 8 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B18'></div>}
+      {p2_count > 7 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B19'></div>}
+      {p2_count > 6 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B20'></div>}
+      {p2_count > 5 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B21'></div>}
+      {p2_count > 4 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B22'></div>}
+      {p2_count > 3 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B23'></div>}
+      {p2_count > 2 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B24'></div>}
+      {p2_count > 1 && <div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className='card B25'></div>}
+      
+      {p2_count > -1 && (
+      <>
+      {p2_count > 0 && (<div style={{ backgroundImage: `url(${background2})`,backgroundSize: "cover"}} className= "card B"></div>)}
+        <ReactCardFlip flipDirection="verticle" isFlipped={isFlipped}>
+        <div style={{ backgroundImage: `url(${face2[1]})`, backgroundSize: "cover" }} className="card BC"></div>
+        {p2_count > 100 && (<div style={{ backgroundImage: `url(${background2})`, backgroundSize: "cover" }} className="card B"></div>)}
+      </ReactCardFlip>
+
+      {w2_count > 5 && (<div style={{backgroundImage: `url(${background2})`,backgroundSize: "cover",}}className="card BW1"></div>)}
+      {w2_count > 4 && (<div style={{backgroundImage: `url(${background2})`,backgroundSize: "cover",}}className="card BW2"></div>)}
+      {w2_count > 3 && (<div style={{backgroundImage: `url(${background2})`,backgroundSize: "cover",}}className="card BW3"></div>)}
+      {w2_count > 2 && (<div style={{backgroundImage: `url(${background2})`,backgroundSize: "cover",}}className="card BW4"></div>)}
+      {w2_count > 1 && (<div style={{backgroundImage: `url(${background2})`,backgroundSize: "cover",}}className="card BW5"></div>)}
+      {w2_count > 0 && (<div style={{backgroundImage: `url(${background2})`,backgroundSize: "cover",}}className="card BW6"></div>)}
+      
+      </>
+      )}
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/faces.jsx
@@ -0,0 +1,188 @@
+//test
+import C2 from "./face/2C@3x.png";
+import C3 from "./face/3C@3x.png";
+import C4 from "./face/4C@3x.png";
+import C5 from "./face/5C@3x.png";
+import C6 from "./face/6C@3x.png";
+import C7 from "./face/7C@3x.png";
+import C8 from "./face/8C@3x.png";
+import C9 from "./face/9C@3x.png";
+import CT from "./face/TC@3x.png";
+import CJ from "./face/JC@3x.png";
+import CQ from "./face/QC@3x.png";
+import CK from "./face/KC@3x.png";
+import CA from "./face/AC@3x.png";
+
+import S2 from "./face/2S@3x.png";
+import S3 from "./face/3S@3x.png";
+import S4 from "./face/4S@3x.png";
+import S5 from "./face/5S@3x.png";
+import S6 from "./face/6S@3x.png";
+import S7 from "./face/7S@3x.png";
+import S8 from "./face/8S@3x.png";
+import S9 from "./face/9S@3x.png";
+import ST from "./face/TS@3x.png";
+import SJ from "./face/JS@3x.png";
+import SQ from "./face/QS@3x.png";
+import SK from "./face/KS@3x.png";
+import SA from "./face/AS@3x.png";
+
+import H2 from "./face/2H@3x.png";
+import H3 from "./face/3H@3x.png";
+import H4 from "./face/4H@3x.png";
+import H5 from "./face/5H@3x.png";
+import H6 from "./face/6H@3x.png";
+import H7 from "./face/7H@3x.png";
+import H8 from "./face/8H@3x.png";
+import H9 from "./face/9H@3x.png";
+import HT from "./face/TH@3x.png";
+import HJ from "./face/JH@3x.png";
+import HQ from "./face/QH@3x.png";
+import HK from "./face/KH@3x.png";
+import HA from "./face/AH@3x.png";
+
+import D2 from "./face/2D@3x.png";
+import D3 from "./face/3D@3x.png";
+import D4 from "./face/4D@3x.png";
+import D5 from "./face/5D@3x.png";
+import D6 from "./face/6D@3x.png";
+import D7 from "./face/7D@3x.png";
+import D8 from "./face/8D@3x.png";
+import D9 from "./face/9D@3x.png";
+import DT from "./face/TD@3x.png";
+import DJ from "./face/JD@3x.png";
+import DQ from "./face/QD@3x.png";
+import DK from "./face/KD@3x.png";
+import DA from "./face/AD@3x.png";
+
+
+
+function randomNoRepeats(array) {
+    var copy = array.slice(0); //copy
+    return function() { // return inner function 
+      if (copy.length < 1) { copy = array.slice(0); } //recreate copy if exhausted
+      var index = Math.floor(Math.random() * copy.length); //get random index
+      var item = copy[index]; //get item
+      copy.splice(index, 1);  //remove item
+      return item;            //return item
+    };
+  }
+  
+  class Queue {
+      constructor() {
+          this.items = {}
+          this.frontIndex = 0
+          this.backIndex = 0
+      }
+      enqueue(item) {
+          this.items[this.backIndex] = item
+          this.backIndex++
+          return item + ' inserted'
+      }
+      dequeue() {
+          const item = this.items[this.frontIndex]
+          delete this.items[this.frontIndex]
+          this.frontIndex++
+          return item
+      }
+      peek() {
+          return this.items[this.frontIndex]
+      }
+
+      length(){
+        return this.backIndex - this.frontIndex;
+      }
+
+      get printQueue() {
+          return this.items;
+      }
+  }
+
+const deck1 = new Queue()
+const deck2 = new Queue()
+
+
+var cards = [
+[14, SA],
+[13, SK],
+[12, SQ],
+[11, SJ],
+[10, ST],
+[9, S9],
+[8, S8],
+[7, S7],
+[6, S6],
+[5, S5],
+[4, S4],
+[3, S3],
+[2, S2],
+
+[14, CA],
+[13, CK],
+[12, CQ],
+[11, CJ],
+[10, CT],
+[9, C9],
+[8, C8],
+[7, C7],
+[6, C6],
+[5, C5],
+[4, C4],
+[3, C3],
+[2, C3],
+
+
+[2, H2],
+[3, H3],
+[4, H4],
+[5, H5],
+[6, H6],
+[7, H7],
+[8, H8],
+[9, H9],
+[10, HT],
+[11, HJ],
+[12, HQ],
+[13, HK],
+[14, HA],
+
+[2, D2],
+[3, D3],
+[4, D4],
+[5, D5],
+[6, D6],
+[7, D7],
+[8, D8],
+[9, D9],
+[10, DT],
+[11, DJ],
+[12, DQ],
+[13, DK],
+[14, DA]
+
+]
+
+
+
+for (let i = 0; i < 26; i++) {
+    deck1.enqueue(cards[i]); 
+}
+for (let i = 26; i < 52; i++) {
+    deck2.enqueue(cards[i]);
+}
+
+// var chooser = randomNoRepeats(cards);
+
+// for (let i = 0; i < 26; i++) {
+//     deck1.enqueue(chooser());
+// }
+   
+// for (let i = 0; i < 26; i++) {
+//     deck2.enqueue(chooser());
+// }
+
+export { Queue }
+export { deck1, deck2 };
+
+
+
